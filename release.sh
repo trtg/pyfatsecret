a678c7100b002544c7d0bfbead992fbfdd9de903
Alex Nelson <w.alexnelson@gmail.com>
2021-10-24 00:48:27 -0600

    package 0.4.0

--- release.sh
+++ release.sh
@@ -4,12 +4,12 @@
 # This is based on the deployment guides found at
 # https://packaging.python.org/tutorials/packaging-projects/
 
-echo "setting up build tools"
+echo "> setting up build tools"
 python3 -m pip install --upgrade build
 python3 -m pip install --upgrade twine
 
-echo "creating new build"
+echo "> creating new build"
 python3 -m build
 
-echo "uploading build assets"
-python3 -m twine upload --repository fatsecret dist/*
+echo "> uploading build assets"
+python3 -m twine upload --repository pypi dist/*
